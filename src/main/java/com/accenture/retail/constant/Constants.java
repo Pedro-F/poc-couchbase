a09945a8f854fbd8e64dfb54aa06eda831c9e77f
Pedro F. <pedro.alonso.garcia@accenture.com>
2016-07-28 13:46:19 +0200

    apuntamos a la BBDD de infra (172.30.0.247)

--- src/main/java/com/accenture/retail/constant/Constants.java
+++ src/main/java/com/accenture/retail/constant/Constants.java
@@ -9,7 +9,7 @@ public class Constants {
 	
 	public static final String PROPERTIES_FILENAME = "application.properties";
 	public static final String NODES_KEY = "couchbase.nodes";
-	public static final String NODES_DEFAULT_VALUE = "127.0.0.1";
+	public static final String NODES_DEFAULT_VALUE = "172.30.0.247";
 	public static final String BASE_URL_KEY = "base.url";
 	public static final String BASE_URL_DEFAULT_VALUE = "/retail";
 	public static final String BUCKET_KEY = "bucket";
